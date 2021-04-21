1b470ab114cef045ec434294f620b72bc47abfbc
chenggang <chenggang@redoornetwork.com>
2021-04-21 17:48:42 +0800

    添加前端mock数据

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,34 @@
 const path = require('path')
-const name = 'Vue Typescript Admin'
+
+const devServerPort = 9517 // TODO: get this variable from setting.ts
+const mockServerPort = 9518 // TODO: get this variable from setting.ts
+const name = 'Vue Ts Admin'
 
 module.exports = {
   // TODO: Remember to change publicPath to fit your need
   publicPath: process.env.NODE_ENV === 'production' ? '/vue-typescript-admin-template/' : '/',
   lintOnSave: process.env.NODE_ENV === 'development',
-  pwa: {
-    name: name
+  productionSourceMap: true,
+  devServer: {
+    port: devServerPort,
+    open: true,
+    overlay: {
+        warnings: false,
+        errors: true
+    },
+    progress: false,
+    proxy: {
+      // change xxx-api/login => /mock-api/v1/login
+      // detail: https://cli.vuejs.org/config/#devserver-proxy
+      [process.env.VUE_APP_BASE_API]: {
+        target: `http://127.0.0.1:${mockServerPort}/mock-api/v1`,
+        changeOrigin: true, // needed for virtual hosted sites
+        ws: true, // proxy websockets
+        pathRewrite: {
+          ['^' + process.env.VUE_APP_BASE_API]: ''
+        }
+      }
+    }
   },
   pluginOptions: {
     'style-resources-loader': {
